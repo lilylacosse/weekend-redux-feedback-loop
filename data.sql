7935f7ae329b5d5884ff0b9e7b62152eefce09ab
Lily LaCosse <lilylacosse@gmail.com>
2023-03-31 15:51:13 -0500

    intital setup of redux complete

--- data.sql
+++ data.sql
@@ -1,5 +1,4 @@
 -- Database should be prime_feedback
-
 -- Switch to "prime_feedback" before making:
 -- Table to store the feedback
 CREATE TABLE "feedback" (
@@ -10,8 +9,12 @@ CREATE TABLE "feedback" (
   "comments" text,
   "flagged" boolean default false,
   "date" date not null default CURRENT_DATE
-); 
-
+);
 -- Sample feedback item
-INSERT INTO "feedback" ("feeling", "understanding", "support", "comments")
+INSERT INTO "feedback" (
+    "feeling",
+    "understanding",
+    "support",
+    "comments"
+  )
 VALUES (4, 4, 5, 'Doing Great!');
--- src/index.js
+++ src/index.js
@@ -2,10 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import { Provider } from 'react';
+import { Provider } from 'react-redux';
+// Redux Reducers
+const feedback = (state = {}, action){
+    if (action.type === 'SAVE_FEEDBACK') {
+        return {...state, ...action.payload}
+    }
+    else if (action.type === "CLEAR_FEEDBACK") {
+        return {}
+    }
+}
+// Redux Store
+const storeFeedback = createStore(
+    combineReducers({
+        // reducers 
+        feedback
+    }), applyMiddleware(logger));
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={storeFeedback}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
